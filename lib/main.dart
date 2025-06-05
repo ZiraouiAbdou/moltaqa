108e71bcb887692b57c47984fb8c2f2c910b02fd
ZiraouiAbdou <ziraouia9@gmail.com>
2025-06-05 07:09:27 +0200

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moltaqa/core/routes/app_router.dart';
+import 'package:moltaqa/firebase_options.dart';
 import 'package:moltaqa/moltaqa_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtil.ensureScreenSize();
   runApp(MoltaqaApp(appRouter: AppRouter()));
 }
