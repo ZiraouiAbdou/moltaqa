4b9e15165bdb2a6e2d888214dee187dd1032f109
ZiraouiAbdou <ziraouia9@gmail.com>
2025-09-20 09:52:27 +0200

    added necessary routes

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,15 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:moltaqa/core/routes/routes_constants.dart';
 import 'package:moltaqa/features/auth/ui/auth_redirector.dart';
 import 'package:moltaqa/features/auth/ui/auth_screen.dart';
 import 'package:moltaqa/features/auth/ui/forgot_password_screen.dart';
+import 'package:moltaqa/features/home/ui/online_room/answer_verification_screen.dart';
+import 'package:moltaqa/features/home/ui/online_room/leaderboard_screen.dart';
+import 'package:moltaqa/features/home/ui/online_room/lobby_screen.dart';
+import 'package:moltaqa/features/home/ui/online_room/question_screen.dart';
 import 'package:moltaqa/features/moltaqa_nav_bar/bottom_nav_bar_screen.dart';
 import 'package:moltaqa/features/home/ui/home_screen.dart';
 import 'package:moltaqa/features/packs/ui/packs_screen.dart';
 import 'package:moltaqa/features/profile/ui/profile_screen.dart';
 
 class AppRouter {
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  static final _shellHomeKey = GlobalKey<NavigatorState>(
+    debugLabel: 'shell Home',
+  );
+  static final _shellPacksKey = GlobalKey<NavigatorState>(
+    debugLabel: 'shell Packs',
+  );
+  static final _shellProfileKey = GlobalKey<NavigatorState>(
+    debugLabel: 'shell Profile',
+  );
+
   final GoRouter router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
     routes: <RouteBase>[
       GoRoute(
         path: RoutesConstants.authRedirector,
@@ -24,25 +41,41 @@ class AppRouter {
         builder: (context, state) => ForgotPasswordScreen(),
       ),
 
-      // GoRoute(
-      //   path: RoutesConstants.homeScreen,
-      //   builder: (context, state) => HomeScreen(),
-      // ),
+      // ONLINE ROOM ROUTES
+      GoRoute(
+        path: RoutesConstants.lobbyScreen,
+        builder: (context, state) => LobbyScreen(),
+      ),
+      GoRoute(
+        path: RoutesConstants.questionScreen,
+        builder: (context, state) => QuestionScreen(),
+      ),
+      GoRoute(
+        path: RoutesConstants.leaderboardScreen,
+        builder: (context, state) => LeaderboardScreen(),
+      ),
+      GoRoute(
+        path: RoutesConstants.answerVerificationScreen,
+        builder: (context, state) => AnswerVerificationScreen(),
+      ),
+
       StatefulShellRoute.indexedStack(
         builder:
             (context, state, navigationShell) =>
                 MoltaqaScaffoldWithNavBar(navigationShell: navigationShell),
         branches: [
           StatefulShellBranch(
+            navigatorKey: _shellHomeKey,
             routes: [
               GoRoute(
                 path: RoutesConstants.homeScreen,
                 builder: (context, state) => HomeScreen(),
-                routes: [],
               ),
             ],
           ),
+
           StatefulShellBranch(
+            navigatorKey: _shellPacksKey,
             routes: [
               GoRoute(
                 path: RoutesConstants.packsScreen,
@@ -52,6 +85,7 @@ class AppRouter {
             ],
           ),
           StatefulShellBranch(
+            navigatorKey: _shellProfileKey,
             routes: [
               GoRoute(
                 path: RoutesConstants.profileScreen,
--- lib/core/routes/routes_constants.dart
+++ lib/core/routes/routes_constants.dart
@@ -5,4 +5,8 @@ class RoutesConstants {
   static const String forgotPassword = "/forgotPassword";
   static const String packsScreen = "/packsScreen";
   static const String profileScreen = "/profileScreen";
+  static const String lobbyScreen = "/lobbyScreen";
+  static const String questionScreen = "/questionScreen";
+  static const String leaderboardScreen = "/leaderboardScreen";
+  static const String answerVerificationScreen = "/answerVerificationScreen";
 }
